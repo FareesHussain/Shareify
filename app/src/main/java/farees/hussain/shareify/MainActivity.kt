589a1a740f9f3c7f5c62ece8be7a1ef9ab6fb350
Farees Hussain <fareezzhussain@gmail.com>
2020-10-12 13:48:40 +0530

    ui mode and added settings fragment

--- app/src/main/java/farees/hussain/shareify/MainActivity.kt
+++ app/src/main/java/farees/hussain/shareify/MainActivity.kt
@@ -1,8 +1,12 @@
 package farees.hussain.shareify
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
 import farees.hussain.shareify.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +18,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.bottomNavView.apply {
             background = null
-            menu.getItem(1).isEnabled = false
+            menu.getItem(2).isEnabled = false
+        }
+        Log.d("darkmode", "${resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK}")
+        if(resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == 16){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         }
     }
 }
