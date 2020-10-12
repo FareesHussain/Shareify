dde6fd56a25099b94f58b5da0ebc402e813d9996
Farees Hussain <fareezzhussain@gmail.com>
2020-10-12 12:57:05 +0530

    bottom app bar and custom action bar

--- app/src/main/java/farees/hussain/shareify/MainActivity.kt
+++ app/src/main/java/farees/hussain/shareify/MainActivity.kt
@@ -2,10 +2,19 @@ package farees.hussain.shareify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import farees.hussain.shareify.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavView.apply {
+            background = null
+            menu.getItem(1).isEnabled = false
+        }
     }
 }
